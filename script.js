23f98c94ef5ba880ee31de2da822f36cb826ab76
Bohdan Savchuk <leonidovich1804@gmail.com>
2020-03-13 22:55:36 +0200

    gift fixes

--- script.js
+++ script.js
@@ -238,23 +238,26 @@ document.addEventListener('DOMContentLoaded', () => {
         const fixedGift = document.querySelector('.fixed-gift'),
             giftPopup = document.getElementById('gift');
 
-        let overlay = giftPopup.querySelector('.overlay'),
-            close = giftPopup.querySelector('.close_icon');
+        if(fixedGift) {
 
-        document.addEventListener('click', () => {
-           let target = event.target;
+            let overlay = giftPopup.querySelector('.overlay'),
+                close = giftPopup.querySelector('.close_icon');
 
-            console.log(target);
-
-            if(target.closest('.fixed-gift')){
-                giftPopup.style.display = 'block';
-                fixedGift.style.display = 'none';
-            }
-
-            if(target.contains(overlay) || target.contains(close) || target.matches('.close-btn')) {
-                giftPopup.style.display = 'none';
-            }
-        });
+            document.addEventListener('click', () => {
+                let target = event.target;
+     
+                 if(target.closest('.fixed-gift')){
+                     giftPopup.style.display = 'block';
+                     fixedGift.style.display = 'none';
+                 }
+     
+                 if(target.contains(overlay) || target.contains(close) || target.matches('.close-btn')) {
+                     giftPopup.style.display = 'none';
+                 }
+             });
+        } else {
+            return;
+        }
     }; 
 
     getGift();
