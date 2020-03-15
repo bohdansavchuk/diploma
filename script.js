82760885b566975ce74bb225e73ad904f660c3c7
Bohdan Savchuk <leonidovich1804@gmail.com>
2020-03-15 13:39:16 +0200

    modals form

--- script.js
+++ script.js
@@ -324,7 +324,6 @@ document.addEventListener('DOMContentLoaded', () => {
                         }
                         cancelAnimationFrame(spinInterval);
                         statusMessage.style.transform = 'unset';
-                        thanksPopup.style.display = 'block';
                         let inputs = item.querySelectorAll('input');
                         inputs.forEach((item) => {
                             if(item.type !== 'radio' && item.type !== 'hidden') {
@@ -338,18 +337,36 @@ document.addEventListener('DOMContentLoaded', () => {
                             });
                         };
                         setTimeout(inStart, 3000);
-                        
+                        if(item.id === 'form4' || item.id === 'form5'){
+                            let content = item.parentNode;
+                            content.innerHTML = `
+                            <h4>Спасибо!</h4>
+                            <p style="font-size: 20px; color: #ffffff; flex-basis: 60%; padding: 20px">
+                            Ваша заявка отправлена. <br> Мы с Вами свяжемся в ближайшее время!</p>
+                            `;
+                        } else {
+                            thanksPopup.style.display = 'block';
+                        }
                     })
                     .catch((error) => {
                         cancelAnimationFrame(spinInterval);
                         statusMessage.style.transform = 'unset';
-                        thanksPopup.style.display = 'block';
-                        popupContent.innerHTML = `
-                        <h4>Ошибка!</h4>
-                        <p>Ваша заявка не отправлена. <br> Заполните форму ещё раз, пожалуйста!</p>
-                        <button class="btn close-btn" wfd-id="216">OK</button>
-                        `;
                         console.error(error);
+                        if(item.id === 'form4' || item.id === 'form5'){
+                            let content = item.parentNode;
+                            content.innerHTML = `
+                            <h4>Ошибка!</h4>
+                            <p style="font-size: 20px; color: #ffffff; flex-basis: 60%; padding: 20px">
+                            Ваша заявка не отправлена.</p>
+                            `;
+                        } else {
+                            thanksPopup.style.display = 'block';
+                            popupContent.innerHTML = `
+                            <h4>Ошибка!</h4>
+                            <p>Ваша заявка не отправлена. <br> Заполните форму ещё раз, пожалуйста!</p>
+                            <button class="btn close-btn" wfd-id="216">OK</button>
+                            `;
+                        }
                     });
             });
         });
@@ -462,7 +479,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             });
 
-        };
+        }
     };
 
     calc();
